2b99e6ff5df8070fbd26426f4d07e4f20a2a7f35
Jose Silva <j.alberto.silva23@gmail.com>
2022-07-06 20:17:01 +0100

    first commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <section>
+      <video autoPlay muted loop>
+        <source src="pexels-karolina-grabowska-5243381.mp4" />
+      </video>
+    </section>
+  );
+}
