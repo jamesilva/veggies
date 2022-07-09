9fc299c8d8df108d3acf322a386e4f3b3fce742a
Jose Silva <j.alberto.silva23@gmail.com>
2022-07-09 16:45:08 +0100

    remove motion divs from login and join

--- app/routes/join.tsx
+++ app/routes/join.tsx
@@ -99,94 +99,86 @@ export default function Join() {
   }, [actionData]);
 
   return (
-    <motion.main
-      key={useLocation().key}
-      className="h-full pt-16"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
-      <div className="flex min-h-full flex-col justify-center">
-        <div className="mx-auto  w-full max-w-md px-8">
-          <Form method="post" className="space-y-6">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <div className="mt-1">
-                <input
-                  ref={emailRef}
-                  id="email"
-                  required
-                  autoFocus={true}
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  aria-invalid={actionData?.errors?.email ? true : undefined}
-                  aria-describedby="email-error"
-                  className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-                />
-                {actionData?.errors?.email && (
-                  <div className="pt-1 text-red-700" id="email-error">
-                    {actionData.errors.email}
-                  </div>
-                )}
-              </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
-              <div className="mt-1">
-                <input
-                  id="password"
-                  ref={passwordRef}
-                  name="password"
-                  type="password"
-                  autoComplete="new-password"
-                  aria-invalid={actionData?.errors?.password ? true : undefined}
-                  aria-describedby="password-error"
-                  className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-                />
-                {actionData?.errors?.password && (
-                  <div className="pt-1 text-red-700" id="password-error">
-                    {actionData.errors.password}
-                  </div>
-                )}
-              </div>
-            </div>
-
-            <input type="hidden" name="redirectTo" value={redirectTo} />
-            <button
-              type="submit"
-              className="w-full rounded bg-teal-600  py-2 px-4 text-white hover:bg-teal-700 focus:bg-teal-500"
+    <div className="flex min-h-full flex-col justify-center">
+      <div className="mx-auto  w-full max-w-md px-8">
+        <Form method="post" className="space-y-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
             >
-              Criar Conta
-            </button>
-            <div className="flex items-center justify-center">
-              <div className="text-center text-sm text-gray-500">
-                Já tem uma conta?{" "}
-                <Link
-                  className="text-blue-500 underline"
-                  to={{
-                    pathname: "/login",
-                    search: searchParams.toString(),
-                  }}
-                >
-                  Entrar
-                </Link>
-              </div>
+              Email
+            </label>
+            <div className="mt-1">
+              <input
+                ref={emailRef}
+                id="email"
+                required
+                autoFocus={true}
+                name="email"
+                type="email"
+                autoComplete="email"
+                aria-invalid={actionData?.errors?.email ? true : undefined}
+                aria-describedby="email-error"
+                className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+              />
+              {actionData?.errors?.email && (
+                <div className="pt-1 text-red-700" id="email-error">
+                  {actionData.errors.email}
+                </div>
+              )}
             </div>
-          </Form>
-        </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <div className="mt-1">
+              <input
+                id="password"
+                ref={passwordRef}
+                name="password"
+                type="password"
+                autoComplete="new-password"
+                aria-invalid={actionData?.errors?.password ? true : undefined}
+                aria-describedby="password-error"
+                className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+              />
+              {actionData?.errors?.password && (
+                <div className="pt-1 text-red-700" id="password-error">
+                  {actionData.errors.password}
+                </div>
+              )}
+            </div>
+          </div>
+
+          <input type="hidden" name="redirectTo" value={redirectTo} />
+          <button
+            type="submit"
+            className="w-full rounded bg-teal-600  py-2 px-4 text-white hover:bg-teal-700 focus:bg-teal-500"
+          >
+            Criar Conta
+          </button>
+          <div className="flex items-center justify-center">
+            <div className="text-center text-sm text-gray-500">
+              Já tem uma conta?{" "}
+              <Link
+                className="text-blue-500 underline"
+                to={{
+                  pathname: "/login",
+                  search: searchParams.toString(),
+                }}
+              >
+                Entrar
+              </Link>
+            </div>
+          </div>
+        </Form>
       </div>
-    </motion.main>
+    </div>
   );
 }
