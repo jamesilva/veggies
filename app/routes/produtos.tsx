00aa632130151017dbd1ca3fd235421ae601ea81
Jose Silva <j.alberto.silva23@gmail.com>
2022-07-21 15:14:06 +0100

    add breadcrumb to product categoty

--- app/routes/produtos.tsx
+++ app/routes/produtos.tsx
@@ -1,6 +1,11 @@
 import type { LoaderFunction, MetaFunction } from "@remix-run/node";
-import { Link, Outlet, useParams } from "@remix-run/react";
+import { Link, Outlet, useMatches, useParams } from "@remix-run/react";
 import { getAllProducts } from "~/models/product.server";
+import { useMatchesData } from "~/utils";
+
+import type { Product } from "~/models/product.server";
+
+type productData = { product: Product };
 
 export type LoaderData = {
   products: Awaited<ReturnType<typeof getAllProducts>>;
@@ -16,21 +21,26 @@ export let meta: MetaFunction = () => ({
 
 export default function Products() {
   let { category } = useParams();
+  let productData = useMatchesData(
+    "routes/produtos/$category/$productId"
+  ) as productData;
 
   return (
     <section className="px-[3vw]">
-      <header className="py-4 md:col-start-2">
-        <ol className="inline-flex gap-x-4">
+      <header className="py-4">
+        <ol className="inline-flex gap-x-4" aria-label="category navigation">
           <li>
             <Link to=".">Categorias</Link>
           </li>
           {category ? (
             <li>
-              {" "}
               <Link className="capitalize" to={`${category}`}>
                 {category}
               </Link>
             </li>
+          ) : null}
+          {productData ? (
+            <li className="capitalize"> {productData.product.name}</li>
           ) : null}
         </ol>
       </header>
